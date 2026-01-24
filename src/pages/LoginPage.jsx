c180c3122481b2a37a664aa0f544d6fa724e1574
Sarvesh <>
2026-01-24 13:27:51 +0530

    login and signup done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../axios/axiosInstance";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [allUsers, setAllUsers] = useState([]);
+
+  const navigate = useNavigate();
+
+  async function getAllSignupUsers() {
+    let resp = await api.get("/users");
+    console.log(resp.data);
+    setAllUsers(resp.data);
+  }
+
+  useEffect(() => {
+    getAllSignupUsers();
+  }, []);
+
+  function checkIsSignedUpUser() {
+    let user = allUsers.find((user) => {
+      return user.email === email && user.password === password;
+    });
+
+    if (user) {
+      return true;
+    }
+    return false;
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    let isPresent = checkIsSignedUpUser();
+
+    if (isPresent) {
+      alert("Login Success 🚀");
+      sessionStorage.setItem("accessToken", Math.random());
+    } else {
+      alert("Login Failed ❌");
+    }
+  };
+
   return (
     <div className="bg-white w-full h-screen flex justify-center ">
       <section className="p-5 mt-20">
         <h1 className="text-xl font-bold text-center">Login</h1>
 
-        <form className="flex flex-col gap-5 p-4">
+        <form className="flex flex-col gap-5 p-4" onSubmit={handleLogin}>
           <input
             className="p-2 outline-0 border border-gray-200"
             type="text"
             name="email"
             id="email"
             placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="p-2 outline-0 border border-gray-200"
@@ -20,6 +63,8 @@ const LoginPage = () => {
             name="password"
             id="password"
             placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="bg-violet-600 p-2 rounded hover:bg-violet-700 text-white font-semibold">
             Login
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,16 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../axios/axiosInstance";
+
 const SignupPage = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    if (
+      username.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === ""
+    ) {
+      alert("All fields are required");
+      return;
+    }
+
+    let newUser = { username, email, password };
+
+    let res = await api.post("/users", newUser);
+    // console.log(res);
+
+    if (res.status === 201) {
+      alert("Signup Success 🚀");
+      navigate("/");
+    } else {
+      alert("Signup Failed ❌");
+    }
+  };
+
   return (
     <div className="bg-white w-full h-screen flex justify-center ">
       <section className="p-5 mt-20">
         <h1 className="text-xl font-bold text-center">Sign up</h1>
 
-        <form className="flex flex-col gap-5 p-4">
+        <form className="flex flex-col gap-5 p-4" onSubmit={handleSignup}>
           <input
             className="p-2 outline-0 border border-gray-200"
             type="text"
             name="username"
             id="username"
             placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <input
             className="p-2 outline-0 border border-gray-200"
@@ -18,6 +55,8 @@ const SignupPage = () => {
             name="email"
             id="email"
             placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="p-2 outline-0 border border-gray-200"
@@ -25,6 +64,8 @@ const SignupPage = () => {
             name="password"
             id="password"
             placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="bg-violet-600 p-2 rounded hover:bg-violet-700 text-white font-semibold">
             Sign up
